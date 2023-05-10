4513242e0091490b5c1a65bfb93149ad2682f16f
sitarcik <jozef.sitarcik@geneton.sk>
2023-05-10 11:05:29 +0200

    test: expanded test Snakefile with more params

--- wrappers/cutadapt/single/wrapper.py
+++ wrappers/cutadapt/single/wrapper.py
@@ -1,6 +1,6 @@
 from snakemake.shell import shell
 
-log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 extra = snakemake.params.get("extra", "")
 
