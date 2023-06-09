f79fb4aeeefe9c340d05c9e972c66ad049faf9e4
sitarcik <jozef.sitarcik@geneton.sk>
2023-06-09 14:55:13 +0200

    fix: fastqc can be called with the memory parameter

--- wrappers/fastqc/quality/wrapper.py
+++ wrappers/fastqc/quality/wrapper.py
@@ -5,17 +5,22 @@ from snakemake.shell import shell
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
+# fastqc allocates memory per thread, so we need to divide the allocated memory per thread
+total_memory = snakemake.resources.get("mem_mb", 0)
+thread_memory = int(total_memory / snakemake.threads)
+memory_arg = ""
+if thread_memory != 0:
+    memory_arg = f"--memory {thread_memory}"
+
+PRESUMED_SUFFIX = ".fastq.gz"
+if not snakemake.input.read.endswith(PRESUMED_SUFFIX):
+    raise ValueError(f"{snakemake.input.read} does not ends with {PRESUMED_SUFFIX}")
+
 
 with tempfile.TemporaryDirectory() as tmpdir:
-    shell(
-        "fastqc --outdir {tmpdir} --extract --dir {tmpdir} --threads {snakemake.threads} {snakemake.input.read} {log}"
-    )
+    shell("fastqc --outdir {tmpdir} --extract --threads {snakemake.threads} {memory_arg} {snakemake.input.read} {log}")
 
-    PRESUMED_SUFFIX = ".fastq.gz"
-    if not snakemake.input.read.endswith(PRESUMED_SUFFIX):
-        raise ValueError(f"{snakemake.input.read} does not ends with {PRESUMED_SUFFIX}")
-
-    base_name = os.path.basename(snakemake.input.read).replace(".fastq.gz", "")
+    base_name = os.path.basename(snakemake.input.read).replace(PRESUMED_SUFFIX, "")
     html_path = os.path.join(tmpdir, f"{base_name}_fastqc.html")
     zip_path = os.path.join(tmpdir, f"{base_name}_fastqc.zip")
 
