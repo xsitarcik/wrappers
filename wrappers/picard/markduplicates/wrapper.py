f8b45cd0837f0c849e442a4d789d04e08f42bae1
sitarcik <jozef.sitarcik@geneton.sk>
2023-06-27 15:49:49 +0200

    fix: picard markduplicates using java mem arg

--- wrappers/picard/markduplicates/wrapper.py
+++ wrappers/picard/markduplicates/wrapper.py
@@ -4,10 +4,20 @@ from snakemake.shell import shell
 
 log = snakemake.log_fmt_shell()
 
+mem_mb = snakemake.resources.get("mem_mb", 0)
+if not mem_mb:
+    mem_mb = snakemake.resources.get("mem_gb", 0) * 1024
+
+java_mem_arg = ""
+if mem_mb:
+    java_mem_mb = round(0.75 * mem_mb)
+    java_mem_arg = "-Xmx{}M".format(java_mem_mb)
+
 with tempfile.TemporaryDirectory() as tmpdir:
     shell(
         "("
         " picard MarkDuplicates"
+        " {java_mem_arg}"
         " I={snakemake.input.bam}"
         " O={snakemake.output.bam}"
         " M={snakemake.output.stat}"
