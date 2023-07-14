fdc00f7b1856466eefcb100a993a7eb6de13f884
kucharik <marcel.kucharik@geneton.sk>
2023-07-14 16:10:45 +0200

    Bowtie2 version matched to nipt-heart. #should not be merged to main

--- wrappers/bowtie2/map/wrapper.py
+++ wrappers/bowtie2/map/wrapper.py
@@ -1,5 +1,3 @@
-import os
-
 from snakemake.shell import shell
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
@@ -10,12 +8,10 @@ if len(snakemake.input.reads) == 1:
 else:
     input_arg = f"-1 {snakemake.input.reads[0]} -2 {snakemake.input.reads[1]}"
 
-index = os.path.splitext(snakemake.input.index[0])[0]
 extra = snakemake.params.get("extra", "")
-
 shell(
     "(bowtie2 "
-    " -x {index}"
+    " -x {snakemake.params.prefix}"
     " {input_arg}"
     " --threads {snakemake.threads}"
     " {extra}"
