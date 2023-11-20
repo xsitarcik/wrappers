1a163c3a936e3858b6aa98d5836eddce405ec7ca
sitarcik <jozef.sitarcik@geneton.sk>
2023-11-20 09:51:16 +0100

    feat: added fastuniq for deduplication of reads

---
+++ wrappers/fastuniq/paired/wrapper.py
@@ -0,0 +1,26 @@
+from snakemake.shell import shell
+
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+
+unzipped_in_r1 = snakemake.input.r1.removesuffix(".gz")
+unzipped_in_r2 = snakemake.input.r2.removesuffix(".gz")
+
+inputs = "{unzipped_in_r1}\n{unzipped_in_r2}"
+
+shell(
+    "("
+    " pigz --decompress --keep --processes {snakemake.threads} {snakemake.input.r1}"
+    " &&"
+    " pigz --decompress --keep --processes {snakemake.threads} {snakemake.input.r2}"
+    " &&"
+    " echo {inputs:q} > {snakemake.output.pair_description}"
+    " &&"
+    " fastuniq -i {snakemake.output.pair_description} -o {snakemake.output.unzipped_out_r1} -p {snakemake.output.unzipped_out_r2}"
+    " &&"
+    " rm {unzipped_in_r1} && {unzipped_in_r2}"
+    " &&"
+    " pigz {snakemake.output.unzipped_out_r1} -9 -c -p {snakemake.threads} > {snakemake.output.r1}"
+    " &&"
+    " pigz {snakemake.output.unzipped_out_r2} -9 -c -p {snakemake.threads} > {snakemake.output.r2}"
+    " ) {log}"
+)
